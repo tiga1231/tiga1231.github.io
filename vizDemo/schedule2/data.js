dbd45dcff3613581651fac3c95ae371bc895c0da
Jack <jack@email.com>
2017-01-14 13:37:28 -0700

    all schedule

---
+++ vizDemo/schedule2/data.js
@@ -0,0 +1,307 @@
+var data = 
+[
+  {
+    "start": "8:00", 
+    "tag": [
+      "CSC 533", 
+      "Comp Graphics", 
+      "GS 701", 
+      "Elton Amburn"
+    ], 
+    "end": "9:15", 
+    "day": "2"
+  }, 
+  {
+    "start": "8:00", 
+    "tag": [
+      "CSC 533", 
+      "Comp Graphics", 
+      "GS 701", 
+      "Elton Amburn"
+    ], 
+    "end": "9:15", 
+    "day": "4"
+  }, 
+  {
+    "start": "15:30", 
+    "tag": [
+      "CSC 665", 
+      "Data Science", 
+      "BioSci W 212", 
+      "Carlos Scheidegger"
+    ], 
+    "end": "16:45", 
+    "day": "2"
+  }, 
+  {
+    "start": "15:30", 
+    "tag": [
+      "CSC 665", 
+      "Data Science", 
+      "BioSci W 212", 
+      "Carlos Scheidegger"
+    ], 
+    "end": "16:45", 
+    "day": "4"
+  }, 
+  {
+    "start": "11:00", 
+    "tag": [
+      "MATH 515B", 
+      "Abstract Algebra", 
+      "C E Chavez 105", 
+      "Robert Indik"
+    ], 
+    "end": "11:50", 
+    "day": "1"
+  }, 
+  {
+    "start": "11:00", 
+    "tag": [
+      "MATH 515B", 
+      "Abstract Algebra", 
+      "C E Chavez 105", 
+      "Robert Indik"
+    ], 
+    "end": "11:50", 
+    "day": "3"
+  }, 
+  {
+    "start": "11:00", 
+    "tag": [
+      "MATH 515B", 
+      "Abstract Algebra", 
+      "C E Chavez 105", 
+      "Robert Indik"
+    ], 
+    "end": "11:50", 
+    "day": "5"
+  }, 
+  {
+    "start": "9:00", 
+    "tag": [
+      "MATH 532", 
+      "Topological Spaces", 
+      "BioSci W 237", 
+      "Sergey Cherkis"
+    ], 
+    "end": "9:50", 
+    "day": "1"
+  }, 
+  {
+    "start": "9:00", 
+    "tag": [
+      "MATH 532", 
+      "Topological Spaces", 
+      "BioSci W 237", 
+      "Sergey Cherkis"
+    ], 
+    "end": "9:50", 
+    "day": "3"
+  }, 
+  {
+    "start": "9:00", 
+    "tag": [
+      "MATH 532", 
+      "Topological Spaces", 
+      "BioSci W 237", 
+      "Sergey Cherkis"
+    ], 
+    "end": "9:50", 
+    "day": "5"
+  }, 
+  {
+    "start": "12:30", 
+    "tag": [
+      "CSC 573", 
+      "Theory of Computation", 
+      "GS 906", 
+      "Stephen Kobourov"
+    ], 
+    "end": "13:45", 
+    "day": "2"
+  }, 
+  {
+    "start": "12:30", 
+    "tag": [
+      "CSC 573", 
+      "Theory of Computation", 
+      "GS 906", 
+      "Stephen Kobourov"
+    ], 
+    "end": "13:45", 
+    "day": "4"
+  }, 
+  {
+    "start": "14:00", 
+    "tag": [
+      "MATH 464", 
+      "Theory of Probability", 
+      "Phys-Atmos Sci 414", 
+      "Diane Holcomb"
+    ], 
+    "end": "15:15", 
+    "day": "2"
+  }, 
+  {
+    "start": "14:00", 
+    "tag": [
+      "MATH 464", 
+      "Theory of Probability", 
+      "Phys-Atmos Sci 414", 
+      "Diane Holcomb"
+    ], 
+    "end": "15:15", 
+    "day": "4"
+  }, 
+  {
+    "start": "9:30", 
+    "tag": [
+      "MATH 464", 
+      "Theory of Probability", 
+      "Engineering 308", 
+      "Douglas Pickrell"
+    ], 
+    "end": "10:45", 
+    "day": "2"
+  }, 
+  {
+    "start": "9:30", 
+    "tag": [
+      "MATH 464", 
+      "Theory of Probability", 
+      "Engineering 308", 
+      "Douglas Pickrell"
+    ], 
+    "end": "10:45", 
+    "day": "4"
+  }, 
+  {
+    "start": "10:00", 
+    "tag": [
+      "MATH 413", 
+      "Linear Algebra", 
+      "C E Chavez 103", 
+      "Houssam Abdul-rahman"
+    ], 
+    "end": "10:50", 
+    "day": "1"
+  }, 
+  {
+    "start": "10:00", 
+    "tag": [
+      "MATH 413", 
+      "Linear Algebra", 
+      "C E Chavez 103", 
+      "Houssam Abdul-rahman"
+    ], 
+    "end": "10:50", 
+    "day": "3"
+  }, 
+  {
+    "start": "10:00", 
+    "tag": [
+      "MATH 413", 
+      "Linear Algebra", 
+      "C E Chavez 103", 
+      "Houssam Abdul-rahman"
+    ], 
+    "end": "10:50", 
+    "day": "5"
+  }, 
+  {
+    "start": "13:00", 
+    "tag": [
+      "Group Meeting", 
+      "HDC", 
+      "GS 734"
+    ], 
+    "end": "14:00", 
+    "day": "5"
+  }, 
+  {
+    "start": "11:00", 
+    "tag": [
+      "CSC 560", 
+      "Database Systems Implementation", 
+      "GS 906", 
+      "Richard Snodgrass"
+    ], 
+    "end": "12:15", 
+    "day": "1"
+  }, 
+  {
+    "start": "11:00", 
+    "tag": [
+      "CSC 560", 
+      "Database Systems Implementation", 
+      "GS 906", 
+      "Richard Snodgrass"
+    ], 
+    "end": "12:15", 
+    "day": "3"
+  }, 
+  {
+    "start": "9:30", 
+    "tag": [
+      "CSC 544", 
+      "Data Visualization", 
+      "GS 701", 
+      "Joshua Levine"
+    ], 
+    "end": "10:45", 
+    "day": "2"
+  }, 
+  {
+    "start": "9:30", 
+    "tag": [
+      "CSC 544", 
+      "Data Visualization", 
+      "GS 701", 
+      "Joshua Levine"
+    ], 
+    "end": "10:45", 
+    "day": "4"
+  }, 
+  {
+    "start": "13:00", 
+    "tag": [
+      "CSC 252", 
+      "Computer Organization", 
+      "C E Chavez Bldg"
+    ], 
+    "end": "13:50", 
+    "day": "1"
+  }, 
+  {
+    "start": "13:00", 
+    "tag": [
+      "CSC 252", 
+      "Computer Organization", 
+      "C E Chavez Bldg"
+    ], 
+    "end": "13:50", 
+    "day": "3"
+  }, 
+  {
+    "start": "13:00", 
+    "tag": [
+      "CSC 252", 
+      "Computer Organization", 
+      "C E Chavez Bldg"
+    ], 
+    "end": "13:50", 
+    "day": "5"
+  }, 
+  {
+    "start": "16:00", 
+    "tag": [
+      "ECE 509", 
+      "Cyber Security", 
+      "R P Harvill 313", 
+      "Salim Hariri"
+    ], 
+    "end": "18:00", 
+    "day": "4"
+  }
+]
